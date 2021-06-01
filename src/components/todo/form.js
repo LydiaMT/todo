5f23bbc7e19e3c78243ed1ab284e46d74c834646
Lydia Minehan-Tubic <lydiaminehantubic@gmail.com>
2021-06-01 16:23:38 -0700

    code refactored

--- src/components/todo/form.js
+++ src/components/todo/form.js
@@ -1,20 +1,21 @@
 import React,  { useState } from 'react';
 
-function TodoForm (props) {
+function TodoForm(props){
   
   const [item, setItem] = useState({})
 
   const handleInputChange = e => {
-    setItem({ item: {...item, [e.target.name]: e.target.value } });
+    setItem( { ...item, [e.target.name]: e.target.value } );
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     e.target.reset();
     props.handleSubmit(item);
-    const item = {};
-    this.setState({item});
+    // const item = {};
+    setItem({});
   };
+
 
   return (
     <>
--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,37 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoForm from './form.js';
 import TodoList from './list.js';
 
 import './todo.scss';
 
-class ToDo extends React.Component {
+function ToDo(props){
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [],
-    };
-  }
+  const [list, setList] = useState([])
 
-  addItem = (item) => {
+  const handleSubmit = (item) => {
     item._id = Math.random();
     item.complete = false;
-    this.setState({ list: [...this.state.list, item]});
+    setList([...list, item]);
   };
 
-  toggleComplete = id => {
-
-    let item = this.state.list.filter(i => i._id === id)[0] || {};
-
+  const toggleComplete = id => {
+    let item = list.filter(i => i._id === id)[0] || {};
     if (item._id) {
       item.complete = !item.complete;
-      let list = this.state.list.map(listItem => listItem._id === item._id ? item : listItem);
-      this.setState({list});
+      let newList = list.map(listItem => listItem._id === item._id ? item : listItem);
+      setList(newList);
     }
-
   };
 
-  componentDidMount() {
+  const componentDidMount = () =>{
     let list = [
       { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A'},
       { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A'},
@@ -39,32 +31,31 @@ class ToDo extends React.Component {
       { _id: 4, complete: true, text: 'Do Homework', difficulty: 3, assignee: 'Person C'},
       { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B'},
     ];
-
-    this.setState({list});
+    setList(list);
   }
 
-  render() {
-    return (
-      <>
-        <header>
-          <h2>
-          There are {this.state.list.filter(item => !item.complete).length} Items To Complete
-          </h2>
-        </header>
-        <section className="todo">
-          <div>
-            <TodoForm handleSubmit={this.addItem} />
-          </div>
-          <div>
-            <TodoList
-              list={this.state.list}
-              handleComplete={this.toggleComplete}
-            />
-          </div>
-        </section>
-      </>
-    );
-  }
+  console.log(list)
+  return (
+    <>
+      <header>
+        <h2>
+        There are {list.filter(item => !item.complete).length} Items To Complete
+        </h2>
+      </header>
+      <section className="todo">
+        <div>
+          <TodoForm handleSubmit={handleSubmit} />
+        </div>
+        <div>
+          <TodoList
+            list={list}
+            handleComplete={toggleComplete}
+          />
+        </div>
+      </section>
+    </>
+  );
+
 }
 
 export default ToDo;
