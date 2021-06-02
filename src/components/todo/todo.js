f0fa75d17878f9b22d2060b51457bce73d7e9fb2
Lydia Minehan-Tubic <lydiaminehantubic@gmail.com>
2021-06-01 17:07:18 -0700

    useEffect

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TodoForm from './form.js';
 import TodoList from './list.js';
 
@@ -7,6 +7,10 @@ import './todo.scss';
 function ToDo(props){
 
   const [list, setList] = useState([])
+
+  useEffect(() => {
+    {document.title = `To do List: ${list.length}`}
+  })
 
   const handleSubmit = (item) => {
     item._id = Math.random();
@@ -23,18 +27,20 @@ function ToDo(props){
     }
   };
 
-  const componentDidMount = () =>{
-    let list = [
-      { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A'},
-      { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A'},
-      { _id: 3, complete: false, text: 'Walk the Dog', difficulty: 4, assignee: 'Person B'},
-      { _id: 4, complete: true, text: 'Do Homework', difficulty: 3, assignee: 'Person C'},
-      { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B'},
-    ];
-    setList(list);
-  }
+  useEffect(() => {
+    {
+      let list = [
+        { _id: 1, complete: false, text: 'Clean the Kitchen', difficulty: 3, assignee: 'Person A'},
+        { _id: 2, complete: false, text: 'Do the Laundry', difficulty: 2, assignee: 'Person A'},
+        { _id: 3, complete: false, text: 'Walk the Dog', difficulty: 4, assignee: 'Person B'},
+        { _id: 4, complete: true, text: 'Do Homework', difficulty: 3, assignee: 'Person C'},
+        { _id: 5, complete: false, text: 'Take a Nap', difficulty: 1, assignee: 'Person B'},
+      ];
+      setList(list);
+    }
+  }, [])
 
-  console.log(list)
+
   return (
     <>
       <header>
@@ -48,6 +54,7 @@ function ToDo(props){
         </div>
         <div>
           <TodoList
+            // onLoad={componentDidMount}
             list={list}
             handleComplete={toggleComplete}
           />
