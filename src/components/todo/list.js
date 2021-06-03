46b493e8484ef0bfd8c0568b577e3403f168097c
Lydia Minehan-Tubic <lydiaminehantubic@gmail.com>
2021-06-03 00:37:02 -0700

    bootstrap fixes

--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -43,16 +43,27 @@ function TodoList(props){
         </Modal.Dialog>
       </When>  
       {props.list.map((item) => (
-        <Card key={item._id}>
-          <Card.Header>
+        <Card 
+          key={item._id}
+          className="shadow p-3 mb-5 bg-white rounded list"
+          >
+          <Card.Header
+            className="text-secondary"
+          >
           <Badge 
             pill variant={item.complete ? "danger" : "success"}
+            className="m-3"
             onClick={() => props.toggleComplete(item._id)}  
             >
             {item.complete===true ? `Complete` : `Pending`}
           </Badge>
-            {item.assignee}
-            <Button variant="light" type="submit" onClick={()=> props.deleteItem(item._id)}>X</Button>
+          <span className="font-weight-bold">{item.assignee}</span>
+            <Button 
+              variant="light" 
+              type="submit"
+              onClick={()=> props.deleteItem(item._id)}
+              className="float-right text-secondary font-weight-bold"
+              >X</Button>
           </Card.Header>
           <Card.Body>
             <Card.Text
@@ -62,7 +73,9 @@ function TodoList(props){
               >
               {item.text}
             </Card.Text>
-            <Card.Text>
+            <Card.Text
+              className="text-sm-right"
+              >
               Difficulty: {item.difficulty}
             </Card.Text>
           </Card.Body>
